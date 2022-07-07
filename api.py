f4280c9d4bed3ca51644ec6a1d253b6c4d02b8ea
DivoHub <39216431+DivoHub@users.noreply.github.com>
2022-07-06 20:45:50 -0700

    new functions json_export() and query_user()

--- api.py
+++ api.py
@@ -3,19 +3,28 @@ from bs4 import BeautifulSoup
 import json
 
 
-def database_query(user_input):
+def html_query(input_dict):
     new_request = get("https://www.techpowerup.com/gpu-specs/")
     if not(new_request.ok):
+        print ("Error making HTTP request. \n\n")
         return
+
+def query_user():
+    user_input = input("")
 
 
 
 def main():
     pass
 
-
-
-
+#Export dict object into json file in subfolder "export"
+def json_export(dict_object):
+    print ("Creating JSON file.\n\n")
+    new_file = open('export/data.json', 'x')
+    json_object = json.dumps(dict_object, indent=2)
+    new_file.write(json_object)
+    new_file.close()
+    print ("JSON file created.")
 
 if __name__ == '__main__':
     main()
