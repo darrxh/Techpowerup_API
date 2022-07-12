e84560f4d2fb88aa4714ba719d86a2847cc8e622
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2022-07-11 22:08:26 -0700

    don't really know what I'm doing

--- api.py
+++ api.py
@@ -1,16 +1,30 @@
-from requests import get
+import requests
 from bs4 import BeautifulSoup
 import json
 
 
-def html_query(input_dict):
-    new_request = get("https://www.techpowerup.com/gpu-specs/")
+def html_query():
+    new_request = requests.get("https://www.techpowerup.com/gpu-specs/")
     if not(new_request.ok):
         print ("Error making HTTP request. \n\n")
         return
+    pull_database(new_request)
+
+def sort_database(new_request):
+    html_doc = BeautifulSoup(new_request.text, "html.parser")
+    print (html_doc)
+
+def update_reference():
+    pass
+
+
 
 def query_user():
-    user_input = input("")
+    user_input = str(input("Enter GPU model: \n"))
+    query_object = {"quicksearch": user_input}
+    new_query = requests.post("https://www.techpowerup.com/gpu-specs/", json = query_object)
+
+    print (new_query.text)
 
 
 
@@ -27,6 +41,7 @@ def json_export(dict_object):
     print ("JSON file created.")
 
 if __name__ == '__main__':
-    main()
+    html_query()
+    #main()
 
 
