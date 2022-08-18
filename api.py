00515bd09a73b318a653cc7d3146fb0f89755e2b
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2022-08-17 22:11:10 -0700

    remove redundancies

--- api.py
+++ api.py
@@ -9,48 +9,49 @@ class Gpu():
         self.base_url = "https://www.techpowerup.com/gpu-specs/"
         self.parameters = {
         "mfgr" : ["NVIDIA","AMD","INTEL"],
-        "lower_year" : 2010,
-        "upper_year" : 2023 }
+        "lower_year" : 2020,
+        "upper_year" : 2020 + 1 }
         self.url_list = []
 
     def update_url_list(self):
         self.url_list.clear()
         for year in range(self.parameters["lower_year"],self.parameters["upper_year"]):
             self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][0]}&released={year}&mobile=No")
-            self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=No")
-            self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][2]}&released={year}&mobile=No")
-            self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][0]}&released={year}&mobile=Yes")
-            self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=Yes")
-            self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][2]}&released={year}&mobile=Yes")
+            #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=No")
+            #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][2]}&released={year}&mobile=No")
+            #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][0]}&released={year}&mobile=Yes")
+            #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=Yes")
+            #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][2]}&released={year}&mobile=Yes")
 
         for i in self.url_list: #testing line
             print (i)
+
         self.validate_url_list()
+
+        for i in self.url_list:
+            self.gpu_fetch(i)
 
     def validate_url_list(self):
         for each_url in self.url_list:
-            time.sleep(30)
             if not (requests.get(each_url).ok):
                 print (f"Error with URL: {each_url}")
                 return False
             else:
                 print(f"URL: {each_url} OK")
+                time.sleep(1)
         return True
 
-    def html_query(self):
-        new_request = requests.get(self.base_url)
-        print (new_request.ok)
-        return new_request.ok
 
-    def gpu_fetch(new_request):
-        URL = "https://www.techpowerup.com/gpu-specs/"
-        html_query(URL)
+
+    def gpu_fetch(self, url):
+        new_request = requests.get(url)
         html_text = BeautifulSoup(new_request.text, "html.parser")
         html_text = html_text.find("table", class_="processors")
+        print (html_text)
         parts_list = list(html_text.find_all("a"))
-        for index in range(len(parts_list)):
-            parts_list[index] = "Nvidia " + parts_list[index].string
-        print (parts_list) #testing line
+        # for index in range(len(parts_list)):
+        #     parts_list[index] = "Nvidia " + parts_list[index].string
+        # print (parts_list) #testing line
 
 
     def update_reference():
