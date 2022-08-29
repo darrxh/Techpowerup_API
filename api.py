01dba5602854e239dcbfc68d84bf7f48129285ee
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2022-08-29 15:10:33 -0700

    dict validator function

--- api.py
+++ api.py
@@ -13,28 +13,20 @@ class Gpu():
         "upper_year" : 2020 + 1 }
         self.url_list = []
 
-
     def create_url_list(self):
         for year in range(self.parameters["lower_year"],self.parameters["upper_year"]):
             self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][0]}&released={year}&mobile=No")
-            #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=No")
+            self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=No")
             #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][2]}&released={year}&mobile=No")
             #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][0]}&released={year}&mobile=Yes")
             #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][1]}&released={year}&mobile=Yes")
             #self.url_list.append(f"{self.base_url}?mfgr={self.parameters['mfgr'][2]}&released={year}&mobile=Yes")
-
 
     def update_url_list(self):
         self.url_list.clear()
         self.create_url_list()
         self.print_list(self.url_list) #testing line
         self.validate_url_list()
-
-
-        for i in self.url_list:
-            self.gpu_fetch(i)
-
-
 
     def validate_url_list(self):
         for each_url in self.url_list:
@@ -51,20 +43,19 @@ class Gpu():
             print (item)
         return
 
-
     def dict_format(self, part_object):
         spec_list = part_object.find_all("td")
         core_counts = spec_list[7].string.split("/")
         part_dict = \
-        {"vendor" : spec_list[0]['class'],
-         "model" : spec_list[0].find('a').string,
+        {"vendor" : spec_list[0]['class'][0].replace("vendor-", ""),
+         "model" : str(spec_list[0].find('a').string),
          "url" : spec_list[0].find('a')['href'],
          "chip" : spec_list[1].string,
-         "release_date" : spec_list[2].string,
+         "release_date" : str(spec_list[2].string),
          "bus" : spec_list[3].string,
-         "memory" : spec_list[4],
-         "gpu_clock" : spec_list[5],
-         "memory_clock" : spec_list[6],
+         "memory" : spec_list[4].string,
+         "gpu_clock" : int((spec_list[5]).string.replace(" MHz", "")),
+         "memory_clock" : int((spec_list[6]).string.replace(" MHz", "")),
          "shader" : int(core_counts[0]),
          "tmu" : int(core_counts[1]),
          "rop" : int(core_counts[2])}
@@ -74,15 +65,32 @@ class Gpu():
 
 
     def dict_validator(self, dict_object):
-        pass
+        condition_log = []
+        condition_log.append("NVIDIA" in dict_object["vendor"] or "AMD" in dict_object["vendor"] or "INTEL" in dict_object["vendor"])
+        condition_log.append(type(dict_object["model"]) is str)
+        condition_log.append("gpu-specs" in dict_object["url"])
+        #ondition_log.append(dict_object["chip"])
+        condition_log.append(type(dict_object["release_date"]) is str)
+        condition_log.append("PCIe" in dict_object["bus"])
+        condition_log.append("DDR" in dict_object["memory"] or "bit" in dict_object["memory"])
+        condition_log.append(type(dict_object["gpu_clock"]) is int)
+        condition_log.append(type(dict_object["memory_clock"]) is int)
+        condition_log.append(type(dict_object["shader"]) is int)
+        condition_log.append(type(dict_object["tmu"]) is int)
+        condition_log.append(type(dict_object["rop"]) is int)
 
-    def gpu_fetch(self, url):
-        new_request = requests.get(url)
-        html_text = BeautifulSoup(new_request.text, "html.parser")
-        html_text = html_text.find("table", class_="processors")
-        parts_list = html_text.find_all("tr")
-        del parts_list[0:2]
-        parts_list = list(map(self.dict_format, parts_list))
+        print (condition_log) #testing line
+
+
+
+    def gpu_fetch(self):
+        for url in self.url_list:
+            new_request = requests.get(url)
+            html_text = BeautifulSoup(new_request.text, "html.parser")
+            html_text = html_text.find("table", class_="processors")
+            parts_list = html_text.find_all("tr")
+            del parts_list[0:2]
+            parts_list = list(map(self.dict_format, parts_list))
 
     def update_reference():
         pass
@@ -139,6 +147,7 @@ class Cpu():
 def main():
     api = Gpu()
     api.update_url_list()
+    api.gpu_fetch()
 
 if __name__ == '__main__':
     main()
